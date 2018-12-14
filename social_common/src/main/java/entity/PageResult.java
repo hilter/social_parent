78b36ba52174c4ad89c0a609c85b6c5bcebcdc0e
AmazeLee <911711054@qq.com>
2018-12-14 08:21:05 +0800

    搭建父工程、公用模块、创建返回值对象

---
+++ social_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,40 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * @author 麦客子
+ * @desc 分页结果类
+ * @email leeshuhua@163.com
+ * @create 2018/12/14 7:35
+ **/
+public class PageResult<T> {
+
+    private long total;
+
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+}
---
+++ social_common/src/main/java/entity/StatusCode.java
@@ -0,0 +1,35 @@
+package entity;
+
+/**
+ * @author 麦客子
+ * @desc 状态码实体类
+ * @email leeshuhua@163.com
+ * @create 2018/12/14 8:02
+ **/
+public class StatusCode {
+
+    /**
+     * 成功
+     */
+    public static final int OK=20000;
+    /**
+     * 失败
+     */
+    public static final int ERROR =20001;
+    /**
+     * 用户名或密码错误
+     */
+    public static final int LOGINERROR =20002;
+    /**
+     * 权限不足
+     */
+    public static final int ACCESSERROR =20003;
+    /**
+     * 远程调用失败
+     */
+    public static final int REMOTEERROR =20004;
+    /**
+     * 重复操作
+     */
+    public static final int REPERROR =20005;
+}
